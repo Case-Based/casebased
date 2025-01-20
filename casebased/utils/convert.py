3fbf1379419360daaaf4ff34ecd45f3d0f7dbc32
jonasliendl <57270170+jonasliendl@users.noreply.github.com>
2025-01-20 23:44:23 +0100

    ref: linting

--- casebased/utils/convert.py
+++ casebased/utils/convert.py
@@ -4,10 +4,10 @@ from typing import Optional
 def convert_string_to_type(value: str) -> Optional[type]:
     """
     Convert a string value to a type if it is a built-in type.
-    
+
     Args:
         value (str): The string value to convert to a type.
-        
+
     Returns:
         Optional[type]: The type if it is a built-in type, otherwise None.
     """
