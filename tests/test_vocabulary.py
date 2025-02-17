ed93203406b075235a426d6d8f7968814b2a8d10
jonasliendl <57270170+jonasliendl@users.noreply.github.com>
2025-02-17 01:49:20 +0100

    ref: pre-commit

--- tests/test_vocabulary.py
+++ tests/test_vocabulary.py
@@ -1,26 +1,27 @@
-from casebased.components.vocabulary import Vocabulary, FeatureAttribute, TargetAttribute, Case
 import unittest
+
+from casebased.components.vocabulary import (
+    Case,
+    FeatureAttribute,
+    TargetAttribute,
+    Vocabulary,
+)
 
 TEST_DATA = {
     "features": [
         FeatureAttribute(name="feature1", data_type=int, conditions=[]),
         FeatureAttribute(name="feature2", data_type=int, conditions=[]),
-        FeatureAttribute(name="feature3", data_type=int, conditions=[])
+        FeatureAttribute(name="feature3", data_type=int, conditions=[]),
     ],
     "targets": [
         TargetAttribute(name="target1", data_type=int, conditions=[]),
     ],
     "case": Case(
-        feature_attributes={
-            "feature1": 1,
-            "feature2": 2,
-            "feature3": 3
-        },
-        target_attributes={
-            "target1": 4
-        }
-    )
+        feature_attributes={"feature1": 1, "feature2": 2, "feature3": 3},
+        target_attributes={"target1": 4},
+    ),
 }
+
 
 class TestVocabulary(unittest.TestCase):
     def test__vocabulary_creation(self):
